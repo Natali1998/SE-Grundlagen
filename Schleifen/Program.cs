4f8fe2c959403f3f32e7ced435a7c25552237457
Natali1998 <144698888+Natali1998@users.noreply.github.com>
2023-09-12 11:16:41 +0200

    Init

---
+++ Schleifen/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Schleifen-Demo");
+int iterCount = 0;
+// 1. SchleifenVariante: bei while (true) schreibt "MEEP" so lange bis man es stopt.
+while (iterCount > 0) 
+{
+  Console.WriteLine("MEEP");
+  iterCount = iterCount - 1;
+}
+
+iterCount = 0;
+do
+{
+  Console.WriteLine("DO-WHILE");
+  iterCount--;
+} while (iterCount > 0);
+
+//2. SchleifenVariante:
+Console.WriteLine("FOR-LOOP");
+for (int i = 0; i < 10; i++)
+{
+  Console.WriteLine("FOR");
+
+}
---
+++ Taschenrechner/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Willkommen beim Taschenrechner -> KevinIstDumm <-");
+bool CheckParse1 = false;
+bool CheckParse2 = false;
+var num1 = 0;
+var num2 = 0;
+
+while (!CheckParse1 || !CheckParse2)
+{
+  Console.WriteLine("Bitte gebe Zahl 1 an:");
+  string input1 = Console.ReadLine();
+
+  Console.WriteLine("Bitte gebe Zahl 2 an:");
+  string input2 = Console.ReadLine();
+
+
+  // Konvertierung von Text zu Zahlen
+  CheckParse1 = int.TryParse(input1, out num1);
+  CheckParse2 = int.TryParse(input2, out num2);
+}
+
+
+
+  var result = num1 + num2;
+  Console.WriteLine($"{num1} + {num2} = {result}");
+
+
